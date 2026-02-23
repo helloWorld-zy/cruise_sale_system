9cfc0375fedeaab1895a920f3d37b0c698a43cec
mythcreator <zhuyu198728@gmail.com>
2026-02-23 12:36:36 +0800

    fix: update meilisearch adapter to use ServiceManager

--- backend/internal/pkg/search/meili.go
+++ backend/internal/pkg/search/meili.go
@@ -2,13 +2,14 @@ package search
 
 import "github.com/meilisearch/meilisearch-go"
 
-type MeiliIndexer struct{ client *meilisearch.Client }
+type MeiliIndexer struct{ client meilisearch.ServiceManager }
 
 func NewMeiliIndexer(url, key string) *MeiliIndexer {
-	return &MeiliIndexer{client: meilisearch.NewClient(meilisearch.ClientConfig{Host: url, APIKey: key})}
+	return &MeiliIndexer{client: meilisearch.New(url, meilisearch.WithAPIKey(key))}
 }
 
 func (m *MeiliIndexer) IndexCabin(doc interface{}) error {
-	_, err := m.client.Index("cabins").AddDocuments([]interface{}{doc})
+	task, err := m.client.Index("cabins").AddDocuments([]interface{}{doc}, nil)
+	_ = task // ignore task for simple adapter
 	return err
 }
